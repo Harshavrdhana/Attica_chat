3a46469e0d4467ce4ad82506be118c635f9e1b04
Harshavardhana <vardhanun333@gmail.com>
2024-06-13 12:42:25 +0530

    added files

---
+++ server/src/routes/messageRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createMessage,
+  getMessages,
+  deleteMessage,
+  unreadMessages,
+  markMessagesRead,
+} = require("../controllers/messageController.js");
+const { upload } = require("../middleware/multer.middlewear.js");
+// Adjust the path as necessary
+const result = upload.fields([
+  { name: "image" },
+  { name: "document" },
+  { name: "video" },
+]);
+router.post("/postmessages", result, createMessage);
+router.get("/getmessages/:userId1/:userId2", getMessages);
+router.get('/unread-messages/:userId', unreadMessages);
+router.get('/mark-messages-read/:userId', markMessagesRead);
+
+
+router.delete("/delmessages/:id", deleteMessage);
+
+module.exports = router;
